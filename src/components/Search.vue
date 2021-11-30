39b0b283fe0922141376d9e9f2403e6897d40d4a
liuqinh2s <liuqinh2s@gmail.com>
2021-11-30 21:53:06 +0800

    update

---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <van-search
+    v-model="value"
+    shape="round"
+    background="#4fc08d"
+    placeholder="请输入搜搜关键词"
+    @search="onSearch"
+    @cancel="onCancel"
+  >
+  </van-search>
+  <van-tabs v-model="activeTab">
+    <van-tab title="标签1"><van-empty :description="empty" /></van-tab>
+    <van-tab title="标签2"><van-empty :description="empty" /></van-tab>
+  </van-tabs>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      value: "",
+      activeTab: 1,
+      empty: "你的小宝贝~",
+    };
+  },
+  methods: {
+    onSearch: function () {
+      // Toast(val);
+    },
+    onCancel: function () {
+      // Toast("取消");
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Search, Tab, Tabs, Toast, Empty } from "vant";
+import "vant/lib/index.css";
 
 const app = createApp(App);
-app.use(store).use(router).mount("#app");
+app
+  .use(store)
+  .use(router)
+  .use(Search)
+  .use(Tab)
+  .use(Tabs)
+  .use(Toast)
+  .use(Empty)
+  .mount("#app");
